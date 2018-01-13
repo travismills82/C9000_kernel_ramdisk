30bb280d70b5671f994962c21b094d6214f1a6e1
travis mills <travismills82@gmail.com>
2018-01-13 13:02:51 +0800

    sysinit cleanup

--- bootimg/ramdisk/sbin/sysinit.sh
+++ bootimg/ramdisk/sbin/sysinit.sh
@@ -16,14 +16,14 @@ for FILE in /system/etc/init.d/*; do
 done;
 
 # Set cpu min freq for big and small clusters (400mhz) 
-echo '0:400000' > /sys/module/msm_performance/paramaters/cpu_min_freq 
-echo '1:400000' > /sys/module/msm_performance/paramaters/cpu_min_freq 
-echo '2:400000' > /sys/module/msm_performance/paramaters/cpu_min_freq 
-echo '3:400000' > /sys/module/msm_performance/paramaters/cpu_min_freq 
-echo '4:400000' > /sys/module/msm_performance/paramaters/cpu_min_freq 
-echo '5:400000' > /sys/module/msm_performance/paramaters/cpu_min_freq 
-echo '6:400000' > /sys/module/msm_performance/paramaters/cpu_min_freq 
-echo '7:400000' > /sys/module/msm_performance/paramaters/cpu_min_freq 
+echo 0:400000 > /sys/module/msm_performance/paramaters/cpu_min_freq 
+echo 1:400000 > /sys/module/msm_performance/paramaters/cpu_min_freq 
+echo 2:400000 > /sys/module/msm_performance/paramaters/cpu_min_freq 
+echo 3:400000 > /sys/module/msm_performance/paramaters/cpu_min_freq 
+echo 4:400000 > /sys/module/msm_performance/paramaters/cpu_min_freq 
+echo 5:400000 > /sys/module/msm_performance/paramaters/cpu_min_freq 
+echo 6:400000 > /sys/module/msm_performance/paramaters/cpu_min_freq 
+echo 7:400000 > /sys/module/msm_performance/paramaters/cpu_min_freq 
  
 # Disable core control thermal 
 echo 0 > /sys/module/msm_thermal/core_control/enabled 
@@ -46,10 +46,10 @@ echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
 echo 1 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster 
  
 # IO disable iostats for sdcard and microsd 
-echo '0' > /sys/block/mmcblk0/queue/iostats 
-echo '0' > /sys/block/mmcblk0/queue/add_random 
-echo '0' > /sys/block/mmcblk1/queue/iostats 
-echo '0' > /sys/block/mmcblk1/queue/add_random 
+echo 0 > /sys/block/mmcblk0/queue/iostats 
+echo 0 > /sys/block/mmcblk0/queue/add_random 
+echo 0 > /sys/block/mmcblk1/queue/iostats 
+echo 0 > /sys/block/mmcblk1/queue/add_random 
  
 # Disable Fsync 
-echo 'N' > /sys/module/sync/parameters/fsync_enabled
+echo N > /sys/module/sync/parameters/fsync_enabled
